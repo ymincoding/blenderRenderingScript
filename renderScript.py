c016aad9fb11b54dc29dd5c242650df14e0e63b5
Yunmin Cho <Yunmin.Cho@dfki.de>
2022-12-01 00:03:41 +0100

    Add an argument to get output directory
    Signed-off-by: Yunmin Cho <dbsals0618@naver.com>

--- renderScript.py
+++ renderScript.py
@@ -1,5 +1,8 @@
 
+import argparse
 import bpy
+import os
+import sys
 
 def get_camera():
     for collection in bpy.data.collections:
@@ -8,25 +11,33 @@ def get_camera():
                 return object
     return None
 
-def render_image(output_path:str):
+#-----------------------------------------------------------------------------------------------------------------------
+
+def render_image(output_dir:str):
     bpy.context.scene.frame_set(200)
     bpy.context.scene.render.image_settings.file_format = 'JPEG'
-    bpy.context.scene.render.filepath = output_path
-    print("Output path: ", bpy.context.scene.render.filepath)
+    bpy.context.scene.render.filepath = os.path.join(output_dir, 'output')
     bpy.ops.render.render(write_still=True)
     
 #-----------------------------------------------------------------------------------------------------------------------
 
-def run():
+def run(args):
     print("Hello Blender Script")
 
     camera = get_camera()
     assert camera != None, "There is no camera in blender file."
 
-    output_path = "D:\Yunmin\TUK\WS22_23\Thesis\src\output"
-    render_image(output_path)
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+    render_image(args.output_dir)
 
 #=======================================================================================================================
 
 if __name__ == "__main__":
-    run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_dir', 
+                        required=True, 
+                        help='Output directory to save the results')
+    
+    args = parser.parse_args(sys.argv[sys.argv.index("--") + 1:])
+    run(args)
